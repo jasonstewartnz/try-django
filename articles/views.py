a63a9a553ae1d922c2ce86208d2553bcd1c547d9
Jason Stewart <jason.stewart@gmail.com>
2024-05-11 23:09:11 -0700

    clean up render

--- articles/views.py
+++ articles/views.py
@@ -1,21 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template.loader import render_to_string
-from articles.models import Article
-# Create your views here.
+from .models import Article
+from django.shortcuts import render
 
 
-def article_view(request, id):
-
-    article_obj = Article.objects.get(id=id)
+def article_detail_view(request, id=None):
+    article_obj = None
+    if id is not None:
+        article_obj = Article.objects.get(id=id)
 
     context = {
-        'title':article_obj.title,
-        'id':article_obj.id,
-        'content':article_obj.content
+        'title': article_obj.title,
+        'id': article_obj.id,
+        'content': article_obj.content
     }
 
-
-    HTML_STRING = render_to_string('article-view.html',context=context)
-
-    return HttpResponse( HTML_STRING )
+    return render(request,'articles/detail.html',context=context)
